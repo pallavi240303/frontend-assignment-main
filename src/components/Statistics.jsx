eae264f148695fcb5111a10487bfb065ef3ba987
pallavi240303 <24.pallavi.2003@gmail.com>
2024-08-15 09:40:38 +0530

    .

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -8,7 +8,7 @@ function Statistics() {
   useEffect(() => {
     async function fetchCoinData() {
       try {
-        const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
+        const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
         const data = await response.json();
         setCoinData(data);
       } catch (error) {
